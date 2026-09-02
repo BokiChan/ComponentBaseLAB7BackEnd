661e6b3ba9183750bd7337948809590926595cf7
Boki Chan <onigirisamasan@gmail.com>
2026-09-02 10:29:52 +0700

    fact: add a REST api

---
+++ src/main/java/se331/lab/Lab7BackendApplication.java
@@ -0,0 +1,13 @@
+package se331.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7BackendApplication.class, args);
+    }
+
+}
