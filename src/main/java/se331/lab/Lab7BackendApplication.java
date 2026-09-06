a9cb928c4337d9b2d6764b55b20341d25738de95
Boki Chan <onigirisamasan@gmail.com>
2026-09-06 15:13:01 +0700

    feat: add mapping to front end

--- src/main/java/se331/lab/Lab7BackendApplication.java
+++ src/main/java/se331/lab/Lab7BackendApplication.java
@@ -2,6 +2,9 @@ package se331.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lab7BackendApplication {
@@ -10,4 +13,14 @@ public class Lab7BackendApplication {
         SpringApplication.run(Lab7BackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/events")
+                        .allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
 }
