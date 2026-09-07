aa8f7d89443ecc5e9ca623e21d7e03d55a842053
Boki Chan <onigirisamasan@gmail.com>
2026-09-07 08:17:29 +0700

    feat: add post method for organization

--- src/main/java/se331/lab/config/InitApp.java
+++ src/main/java/se331/lab/config/InitApp.java
@@ -5,13 +5,16 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.lab.entity.Event;
+import se331.lab.entity.Organizer;
 import se331.lab.repository.EventRepository;
+import se331.lab.repository.OrganizerRepository;
 
 
 @Component
 @RequiredArgsConstructor
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     final EventRepository eventRepository;
+    final OrganizerRepository organizerRepository;
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
         eventRepository.save(Event.builder()
@@ -50,6 +53,13 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .time("10.00am-6.00 pm.")
                 .petsAllowed(true)
                 .organizer("Chiang Mai Municipality").build());
-
+        organizerRepository.save(Organizer.builder()
+                .organizationName("CAMT")
+                .address("Chiang Mai University, Chiang Mai")
+                .build());
+        organizerRepository.save(Organizer.builder()
+                .organizationName("Chiang Mai Municipality")
+                .address("Chiang Mai, Thailand")
+                .build());
     }
 }
