94a033930d337d79af94b5a0097502a2c951e127
Boki Chan <onigirisamasan@gmail.com>
2026-09-07 08:05:52 +0700

    feat: add post method

--- src/main/java/se331/lab/dao/EventDaoImpl.java
+++ src/main/java/se331/lab/dao/EventDaoImpl.java
@@ -105,4 +105,11 @@ public class EventDaoImpl implements EventDao {
     public Event getEvent(Long id){
         return eventsList.stream().filter(event -> event.getId().equals(id)).findFirst().orElse(null);
     }
+
+    @Override
+    public Event save(Event event){
+        event.setId(eventsList.get(eventsList.size()-1).getId() + 1);
+        eventsList.add(event);
+        return event;
+    }
 }
