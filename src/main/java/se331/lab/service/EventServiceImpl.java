23916e9c05f1e318e608565c00462d76c232e55e
Boki Chan <onigirisamasan@gmail.com>
2026-09-13 20:10:26 +0700

    feat: 6.5 make event come with organizer

--- src/main/java/se331/lab/service/EventServiceImpl.java
+++ src/main/java/se331/lab/service/EventServiceImpl.java
@@ -1,16 +1,22 @@
 package se331.lab.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.dao.EventDao;
+import se331.lab.dao.OrganizerDao;
 import se331.lab.entity.Event;
+import se331.lab.entity.Organizer;
 
 
 @Service
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
     final EventDao eventDao;
+    final OrganizerDao organizerDao;
     @Override
     public Integer getEventSize() {
         return eventDao.getEventSize();
@@ -27,7 +33,14 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+    @Transactional
     public Event save(Event event) {
+        if (event.getOrganizer() == null || event.getOrganizer().getId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The organizer id is required");
+        }
+        Organizer  organizer = organizerDao.findById(event.getOrganizer().getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Organizer id not found"));
+        event.setOrganizer(organizer);
         return eventDao.save(event);
     }
 }
