a6ba97ac4eeadb4282e5ff2ee72df4975e1a3f2d
Boki Chan <onigirisamasan@gmail.com>
2026-09-13 16:12:59 +0700

    feat: error organizer and participant can't work some how

--- src/main/java/se331/lab/entity/Organizer.java
+++ src/main/java/se331/lab/entity/Organizer.java
@@ -1,9 +1,22 @@
 package se331.lab.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
 import java.util.List;
 
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Organizer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String name;
+    @OneToMany(mappedBy = "organizer")
     List<Event> ownEvents;
 }
--- src/main/java/se331/lab/entity/Participant.java
+++ src/main/java/se331/lab/entity/Participant.java
@@ -1,10 +1,24 @@
 package se331.lab.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String name;
     String telNo;
+    @ManyToMany
     List<Event> eventHistories;
 }
